241abe660152a1890e4a6d8fadb06e2d78af2485
CarlErnest0707 <2025-1-01107@lpu.edu.ph>
2025-10-24 07:38:47 +0800

    Add files via upload

---
+++ 1. For loop porgram that counts numbers by 10 .c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() { 
+    for (int a = 317; a <= 3170; a += 317) {
+        printf("%d\n", a);
+    }
+    return 0;
+}
---
+++ 2. Program that inputs even numbers starting from 0.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    for (int c = 0; c <= 20; c += 4) {
+        printf("%d\n", c);
+    }
+    return 0;
+}
